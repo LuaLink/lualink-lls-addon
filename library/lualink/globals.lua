a17b70eb26a8753003606eb34d40f92a1b5c94dd
Saturn <github.vcnmd@passinbox.com>
2025-09-04 11:15:32 +0000

    Add synchronized and exceptionally stubs

--- library/lualink/globals.lua
+++ library/lualink/globals.lua
@@ -8,3 +8,18 @@ server = {}
 
 ---@type Scheduler
 scheduler = {}
+
+
+--- Ensures that only one thread at a time can access a method or block of code, keeping shared data safe in multithreaded programs.
+--- @param lock java.lang.Object
+--- @param func function
+function synchronized(lock, func) end
+
+--- Ensures that only one thread at a time can access a method or block of code, keeping shared data safe in multithreaded programs. This will use the main Lua state as the lock
+--- @param func function
+function synchronized(func) end
+
+--- Let's you try-catch Java exceptions thrown when called from Lua
+--- @param try function
+--- @param catch fun(throwable: java.lang.Object
+function exceptionally(try: function, catch: function)
